e36014be4f8715243a1b84f759afdfcf16e8d943
Carol Schluter <cfmiller176@gmail.com>
2023-03-09 16:28:14 -0600

    topic form controlled components and submit

--- src/App.js
+++ src/App.js
@@ -3,26 +3,6 @@ import TopicForm from './Components/TopicForm/TopicForm';
 import MessageList from './Components/MessageList/MessageList';
 import './App.css';
 
-/*
-class App2 extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Chit Chat App
-          </p>
-        </header>
-        <main>
-          <TopicForm />
-          <MessageList />
-        </main>
-        <footer></footer>
-      </div>
-    );
-  }
-}
-*/
 function App() {
   return (
     <div className="App">
@@ -30,8 +10,12 @@ function App() {
         <p>
           Chit Chat App 
         </p>
-     
       </header>
+      <main>
+          <TopicForm />
+          <MessageList />
+        </main>
+        <footer>Footer</footer>
     </div>
   );
 }
--- src/Components/TopicForm/TopicForm.js
+++ src/Components/TopicForm/TopicForm.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import './TopicForm.css';
 
 export default function TopicForm() {
     return (
         <>
             <h4>Topic form </h4>
-            <p>Use the form to create your first topic</p>
+            <form action="#" method="GET" className="form">
+                <div className="form-row">
+                <label for="name-field"className="form-label">Name:</label>
+                <input id="name-field" type="text" name="name" required />
+                
+            </div>
+            <div className="form-row">
+                <label for="message-field" className="form-label">Message:</label>
+                <textarea id="message-field" name="message" required></textarea>
+                
+            </div>
+            <div className="form-row">
+                <button type="submit">Send</button>
+
+            </div>
+
+            </form>
         </>
     );
 
